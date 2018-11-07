c1db938b65a192a80811f709999f177f945d1e6b
Marcel Müller <marcel@udiocollective.com>
2018-11-07 14:49:28 -0800

    added tensorflow basic

--- tensorflow-experiments/positive-negative-amazon-music-reviews.py
+++ tensorflow-experiments/positive-negative-amazon-music-reviews.py
@@ -1,7 +1,12 @@
 import tensorflow as tf
-from tensorflow import keras
-
+import tensorflow_hub as hub
+import matplotlib.pyplot as plt
 import numpy as np
+import os
+import pandas as pd
+import re
+import seaborn as sns
+from tensorflow import keras
 
 
 train_data = keras.preprocessing.sequence.pad_sequences(train_data,
